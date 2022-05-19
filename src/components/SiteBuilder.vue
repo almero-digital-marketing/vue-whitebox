ea3472f9c8c92710d3373a3ef77efa4d3c3a1375
dimitarkolev <dk@almero.bg>
2022-05-19 11:03:06 +0000

    Added site builder

---
+++ src/components/SiteBuilder.vue
@@ -0,0 +1,6 @@
+<template>
+    <component v-for="item in items" :key="item.title" :is="components[item.component]" v-bind="{...item}"></component>
+</template>
+<script setup>
+defineProps(['items', 'components'])
+</script>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ExternalLink from './components/ExternalLink.vue'
 import Markdown from './components/Markdown.vue'
 import Metatext from './components/Metatext.vue'
 import PhoneLink from './components/PhoneLink.vue'
+import SiteBuilder from './components/SiteBuilder.vue'
 import { markdown } from './lib/markdown'
 import { metatext, removeMetatext } from './lib/metatext'
 
@@ -10,6 +11,7 @@ export {
     Markdown, 
     Metatext, 
     PhoneLink,
+    SiteBuilder,
     markdown,
     metatext,
     removeMetatext
