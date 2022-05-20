db471317fdb336338e0f3d801275fe0cadd29a55
dimitarkolev <dk@almero.bg>
2022-05-20 13:30:39 +0000

    SiteBuilder

--- src/components/SiteBuilder.vue
+++ src/components/SiteBuilder.vue
@@ -1,6 +1,15 @@
 <template>
-    <component v-for="item in items" :key="item.title" :is="components[item.component]" v-bind="{...item}"></component>
+    <component v-for="(item, index) in items" :key="item.component + index" :is="components[item.component] || item.component" v-bind="{...item}"></component>
 </template>
 <script setup>
-defineProps(['items', 'components'])
+defineProps({
+    items: {
+        type: Array,
+        required: true,
+    },
+    components: {
+        type: Object,
+        default: {}
+    }
+})
 </script>
